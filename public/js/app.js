39921c44bfe65619d505a1394c0d8620f250592d
Cristina Rojas <telematicacristina@gmail.com>
2015-09-18 15:56:09 -0700

    Adding the app for show the menu

--- public/js/app.js
+++ public/js/app.js
@@ -8,57 +8,18 @@
     // Merging data objects
     var objectA = require('json!data/breakfast.json');
     var objectB = require('json!data/lunch.json');
-    var objectC = _.merge({}, objectA, objectB);
 
-    // Options arrays
-    var menuArray = [];
+    var theMenu  = objectA.menu.concat(objectB.menu);
+    console.log(theMenu);
 
-    // DOM variables
-    var dataTable;
-
-    _.forEach(objectC.menu, function(food, optionKey) {
-        menuArray.push(food);
-        console.log(menuArray);
-        addRow(food.platillo, food.tipo, food.price)
-    });
-
-    // Get the table element from tbody
-    dataTable = document.getElementById('data-table').getElementsByTagName('tbody')[0];
-
-    // Removing the first row
-    deleteFirstRow();
-
-    // Functions
-    function addRow(platillo, tipo, price) {
-      // Cloning a new row from the first row.
-      var newRow = dataTable.rows[0].cloneNode(true);
-
-      // Assigning the name and the price in each column
-      newRow.cells[0].innerHTML = platillo;
-      newRow.cells[1].innerHTML = tipo;
-      newRow.cells[2].innerHTML = price;
-
-      // Appending new row
-      dataTable.appendChild(newRow);
+    var newContent = '';
+    for(var i = 0; i < theMenu.length; i++) {
+      newContent += '<p>' + 'Platillo: ' + theMenu[i].platillo + '</p>';
+      newContent += '<p>' + 'Tipo: ' + theMenu[i].tipo + '</p>';
+      newContent += '<p>' + 'Precio: ' + '$ ' + theMenu[i].price + '</p></br>';
     }
 
-    // This function is to remove the first row of the table
-    function deleteFirstRow() {
-      dataTable.deleteRow(0);
-    }
-
-    // This funciton is to show or hide the prices depends on hide-text class.
-    function switchPrices() {
-      var priceElements = document.getElementsByClassName('price');
-
-      for (var i = 0; i <= priceElements.length - 1; i++) {
-        if (priceElements[i].classList.contains('hide-text')) {
-            priceElements[i].classList.remove('hide-text');
-        } else {
-          priceElements[i].classList.add('hide-text');
-        }
-      }
-    }
+    document.getElementById('content').innerHTML = newContent;
 
   });
 })();
